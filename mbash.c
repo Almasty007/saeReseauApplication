c6187be5f0c12b81f58417bd31e7e21be0bdfba2
alennardos <92914160+alennardos@users.noreply.github.com>
2023-01-16 17:11:35 +0100

    test

--- mbash.c
+++ mbash.c
@@ -1,50 +1,26 @@
 #include <stdio.h>
+#include <string.h>
 #include <stdlib.h>
 #include <unistd.h>
+#include <assert.h>
+#define MAXLI 2048
+char cmd[MAXLI];
+char path[MAXLI];
+int pathidx;
+void mbash();
+int main(int argc, char** argv) {
+  while (1) {
+    printf("Commande: ");
+    fgets(cmd, MAXLI, stdin);
+    pid = fork();
+    mbash(cmd);
+  }
+  return 0;
+}
 
-#define MAX_LINE_LENGTH 256
-
-int main() {
-    char input[MAX_LINE_LENGTH];
-
-    while (1) {
-        // Afficher le prompt
-        printf("mbash> ");
-        fgets(input, MAX_LINE_LENGTH, stdin);
-
-        // Retirer le caractère de nouvelle ligne de la fin de la chaîne
-        input[strcspn(input, "\n")] = 0;
-
-        // Séparer la commande et ses arguments
-        char *args[MAX_LINE_LENGTH];
-        char *token = strtok(input, " ");
-        int i = 0;
-        while (token != NULL) {
-            args[i++] = token;
-            token = strtok(NULL, " ");
-        }
-        args[i] = NULL;
-
-        if (strcmp(args[0], "cd") == 0) {
-            // Exécuter la commande cd
-            chdir(args[1]);
-        } else if (strcmp(args[0], "pwd") == 0) {
-            // Exécuter la commande pwd
-            char cwd[MAX_LINE_LENGTH];
-            getcwd(cwd, sizeof(cwd));
-            printf("%s\n", cwd);
-        } else {
-            // Exécuter la commande via execvp
-            pid_t pid = fork();
-            if (pid == 0) {
-                // Exécuter la commande dans le processus fils
-                execvp(args[0], args);
-                exit(0);
-            } else if (args[i-1] != "&") {
-                // Attendre la fin de l'exécution de la commande dans le processus père si le & n'est pas présent
-                wait(NULL);
-            }
-        }
-    }
-    return 0;
+void mbash() {
+if (pid == 0){
+  printf("Execute: %s", cmd);
+  system(cmd);
+  }
 }
