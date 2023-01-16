30e4e318fe86e21e4574f936d3c115bcf9ac1603
alennardos <92914160+alennardos@users.noreply.github.com>
2023-01-16 16:49:46 +0100

    test

--- mbash.c
+++ mbash.c
@@ -1,58 +1,51 @@
 #include <stdio.h>
-#include <string.h>
 #include <stdlib.h>
+#include <string.h>
 #include <unistd.h>
-#include <assert.h>
-#include <signal.h>
-#define MAXLI 2048
-char cmd[MAXLI];
-char path[MAXLI];
-char* commandeEntiere[MAXLI];
-int pathidx;
-int pid;
-void mbash();
-void split();
 
+#define MAX_LINE_LENGTH 256
 
-int main(int argc, char** argv) {
-  while (1) {
-    printf("Commande: ");
-    fgets(cmd, MAXLI, stdin);
-    pid = fork();
-    mbash(cmd);
-    //split();
-  }
-  return 0;
-}
+int main() {
+    char input[MAX_LINE_LENGTH];
 
-void mbash() {
-  if(pid == 0){
-      printf("Execute: %s", cmd);
-      printf("print cmd %%d --> %d \n", *cmd);
-      printf("print cmd %%s --> %s \n", cmd);
-      printf("test %d \n", strncmp(cmd, "exit", 4));
+    while (1) {
+        // Afficher le prompt
+        printf("mbash> ");
+        fgets(input, MAX_LINE_LENGTH, stdin);
 
-      //exit
-      if(strncmp(cmd, "exit", 4) == 0) {
-      //tue le pid du parent
-        kill(getppid(), 15);
+        // Retirer le caractère de nouvelle ligne de la fin de la chaîne
+        input[strcspn(input, "\n")] = 0;
 
-      }
-      //if()
+        // Séparer la commande et ses arguments
+        char *args[MAX_LINE_LENGTH];
+        char *token = strtok(input, " ");
+        int i = 0;
+        while (token != NULL) {
+            args[i++] = token;
+            token = strtok(NULL, " ");
+        }
+        args[i] = NULL;
 
-      else {
-        system(cmd);
-      }
-  }
-
-}
-void split() {
-    char *tmp;
-    int i = 0;
-    tmp = strtok(cmd, " ");
-    while(tmp != NULL) {
-        commandeEntiere[i] = tmp;
-        tmp = strtok(NULL, " ");
-        i++;
+        if (strcmp(args[0], "cd") == 0) {
+            // Exécuter la commande cd
+            chdir(args[1]);
+        } else if (strcmp(args[0], "pwd") == 0) {
+            // Exécuter la commande pwd
+            char cwd[MAX_LINE_LENGTH];
+            getcwd(cwd, sizeof(cwd));
+            printf("%s\n", cwd);
+        } else {
+            // Exécuter la commande via execve
+            pid_t pid = fork();
+            if (pid == 0) {
+                // Exécuter la commande dans le processus fils
+                execve(args[0], args, NULL);
+                exit(0);
+            } else if (args[i-1] != "&") {
+                // Attendre la fin de l'exécution de la commande dans le processus père si le & n'est pas présent
+                wait(NULL);
+            }
+        }
     }
+    return 0;
 }
