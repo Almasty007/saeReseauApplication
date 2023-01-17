1a5f1538c2dae83a96ce88e2e4c223f88d81834f
alennardos <92914160+alennardos@users.noreply.github.com>
2023-01-17 12:31:31 +0100

    code commente

--- mbash.c
+++ mbash.c
@@ -4,46 +4,51 @@
 #include <string.h>
 #include <sys/wait.h>
 
-#define MAX_LINE_LEN 2048
+#define MAXLI 2048
 int pid;
 
 int main() {
-    char cmd[MAX_LINE_LEN];
+    char cmd[MAXLI];
+
+    // la boucle principale
 
     while (1) {
-        printf("mini_bash_>");
-        fgets(cmd, MAX_LINE_LEN, stdin);
 
-        // remove the newline character
+        printf("mini_bash_>");
+
+        // recuperation de la commande de l'utilisateur
+        fgets(cmd, MAXLI, stdin);
         cmd[strcspn(cmd, "\n")] = 0;
 
-        // exit if the cmd is "exit"
+        // la fonction d'arret si la commande vaut exit
         if (strcmp(cmd, "exit") == 0) {
             break;
         }
 
-        // create a child process
-
-        char* argv[MAX_LINE_LEN];
+        // la separation de la commande pour la recuperation des attributs
+        char* argv[MAXLI];
         int argc = 0;
 
-                    // parse the cmd into arguments
-        char* token = strtok(cmd, " ");
-        while (token != NULL) {
-            argv[argc++] = token;
-            token = strtok(NULL, " ");
+        char* tempo = strtok(cmd, " ");
+        while (tempo != NULL) {
+            argv[argc++] = tempo;
+            tempo = strtok(NULL, " ");
         }
         argv[argc] = NULL;
+
+        // le changement de repertoire
 
         if (strcmp(argv[0], "cd") == 0) {
             chdir(argv[1]);
         }else{
 
+        // creation de fils pour exectution des autres commandes
+
         pid = fork();
 
 
         if (pid == 0) {
-            char* fullpath = (char *) malloc(MAX_LINE_LEN);
+            char* fullpath = (char *) malloc(MAXLI);
             sprintf(fullpath,"/bin/%s",argv[0]);
             if(execve(fullpath, argv, NULL) != 0) {
                 perror("execvp");
